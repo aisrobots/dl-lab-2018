70940e3a1e132076be4111ddba8bd4a8fa09f74e
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-10-30 11:31:44 +0100

    add exercise 2

---
+++ exercise2/cnn_mnist.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+
+import argparse
+import gzip
+import json
+import os
+import pickle
+
+import numpy as np
+
+
+def one_hot(labels):
+    """this creates a one hot encoding from a flat vector:
+    i.e. given y = [0,2,1]
+     it creates y_one_hot = [[1,0,0], [0,0,1], [0,1,0]]
+    """
+    classes = np.unique(labels)
+    n_classes = classes.size
+    one_hot_labels = np.zeros(labels.shape + (n_classes,))
+    for c in classes:
+        one_hot_labels[labels == c, c] = 1
+    return one_hot_labels
+
+
+def mnist(datasets_dir='./data'):
+    if not os.path.exists(datasets_dir):
+        os.mkdir(datasets_dir)
+    data_file = os.path.join(datasets_dir, 'mnist.pkl.gz')
+    if not os.path.exists(data_file):
+        print('... downloading MNIST from the web')
+        try:
+            import urllib
+            urllib.urlretrieve('http://google.com')
+        except AttributeError:
+            import urllib.request as urllib
+        url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        urllib.urlretrieve(url, data_file)
+
+    print('... loading data')
+    # Load the dataset
+    f = gzip.open(data_file, 'rb')
+    try:
+        train_set, valid_set, test_set = pickle.load(f, encoding="latin1")
+    except TypeError:
+        train_set, valid_set, test_set = pickle.load(f)
+    f.close()
+
+    test_x, test_y = test_set
+    test_x = test_x.astype('float32')
+    test_x = test_x.astype('float32').reshape(test_x.shape[0], 28, 28, 1)
+    test_y = test_y.astype('int32')
+    valid_x, valid_y = valid_set
+    valid_x = valid_x.astype('float32')
+    valid_x = valid_x.astype('float32').reshape(valid_x.shape[0], 28, 28, 1)
+    valid_y = valid_y.astype('int32')
+    train_x, train_y = train_set
+    train_x = train_x.astype('float32').reshape(train_x.shape[0], 28, 28, 1)
+    train_y = train_y.astype('int32')
+    print('... done loading data')
+    return train_x, one_hot(train_y), valid_x, one_hot(valid_y), test_x, one_hot(test_y)
+
+
+def train_and_validate(x_train, y_train, x_valid, y_valid, num_epochs, lr, num_filters, batch_size):
+    # TODO: train and validate your convolutional neural networks with the provided data and hyperparameters
+
+    return learning_curve, model  # TODO: Return the validation error after each epoch (i.e learning curve) and your model
+
+
+def test(x_test, y_test, model):
+    # TODO: test your network here by evaluating it on the test data
+    return test_error
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_path", default="./", type=str, nargs="?",
+                        help="Path where the results will be stored")
+    parser.add_argument("--input_path", default="./", type=str, nargs="?",
+                        help="Path where the data is located. If the data is not available it will be downloaded first")
+    parser.add_argument("--learning_rate", default=1e-3, type=float, nargs="?", help="Learning rate for SGD")
+    parser.add_argument("--num_filters", default=32, type=int, nargs="?",
+                        help="The number of filters for each convolution layer")
+    parser.add_argument("--batch_size", default=128, type=int, nargs="?", help="Batch size for SGD")
+    parser.add_argument("--epochs", default=12, type=int, nargs="?",
+                        help="Determines how many epochs the network will be trained")
+    parser.add_argument("--run_id", default=0, type=int, nargs="?",
+                        help="Helps to identify different runs of an experiments")
+
+    args = parser.parse_args()
+
+    # hyperparameters
+    lr = args.learning_rate
+    num_filters = args.num_filters
+    batch_size = args.batch_size
+    epochs = args.epochs
+
+    # train and test convolutional neural network
+    x_train, y_train, x_valid, y_valid, x_test, y_test = mnist(args.input_path)
+
+    learning_curve, model = train_and_validate(x_train, y_train, x_valid, y_valid, epochs, lr, num_filters, batch_size)
+
+    test_error = test(x_test, y_test, model)
+
+    # save results in a dictionary and write them into a .json file
+    results = dict()
+    results["lr"] = lr
+    results["num_filters"] = num_filters
+    results["batch_size"] = batch_size
+    results["learning_curve"] = learning_curve
+    results["test_error"] = test_error
+
+    path = os.path.join(args.output_path, "results")
+    os.makedirs(path, exist_ok=True)
+
+    fname = os.path.join(path, "results_run_%d.json" % args.run_id)
+
+    fh = open(fname, "w")
+    json.dump(results, fh)
+    fh.close()
---
+++ exercise2/random_search.py
@@ -0,0 +1,114 @@
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+import hpbandster.core.nameserver as hpns
+
+from hpbandster.optimizers import RandomSearch
+
+import ConfigSpace as CS
+from hpbandster.core.worker import Worker
+import argparse
+
+from cnn_mnist_solution import mnist
+
+
+class MyWorker(Worker):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.x_train, self.y_train, self.x_valid, self.y_valid, self.x_test, self.y_test = mnist("./")
+
+    def compute(self, config, budget, **kwargs):
+        """
+        Evaluates the configuration on the defined budget and returns the validation performance.
+
+        Args:
+            config: dictionary containing the sampled configurations by the optimizer
+            budget: (float) amount of time/epochs/etc. the model can use to train
+        Returns:
+            dictionary with mandatory fields:
+                'loss' (scalar)
+                'info' (dict)
+        """
+        lr = config["learning_rate"]
+        num_filters = config["num_filters"]
+        batch_size = config["batch_size"]
+        epochs = budget
+
+        # TODO: train and validate your convolutional neural networks here
+
+        # TODO: We minimize so make sure you return the validation error here
+        return ({
+            'loss': validation_error,  # this is the a mandatory field to run hyperband
+            'info': {}  # can be used for any user-defined information - also mandatory
+        })
+
+    @staticmethod
+    def get_configspace():
+        config_space = CS.ConfigurationSpace()
+
+        # TODO: Implement configuration space here. See https://github.com/automl/HpBandSter/blob/master/hpbandster/examples/example_5_keras_worker.py  for an example
+
+        return config_space
+
+
+parser = argparse.ArgumentParser(description='Example 1 - sequential and local execution.')
+parser.add_argument('--budget', type=float,
+                    help='Maximum budget used during the optimization, i.e the number of epochs.', default=12)
+parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=20)
+args = parser.parse_args()
+
+# Step 1: Start a nameserver
+# Every run needs a nameserver. It could be a 'static' server with a
+# permanent address, but here it will be started for the local machine with the default port.
+# The nameserver manages the concurrent running workers across all possible threads or clusternodes.
+# Note the run_id argument. This uniquely identifies a run of any HpBandSter optimizer.
+NS = hpns.NameServer(run_id='example1', host='127.0.0.1', port=None)
+NS.start()
+
+# Step 2: Start a worker
+# Now we can instantiate a worker, providing the mandatory information
+# Besides the sleep_interval, we need to define the nameserver information and
+# the same run_id as above. After that, we can start the worker in the background,
+# where it will wait for incoming configurations to evaluate.
+w = MyWorker(nameserver='127.0.0.1', run_id='example1')
+w.run(background=True)
+
+# Step 3: Run an optimizer
+# Now we can create an optimizer object and start the run.
+# Here, we run RandomSearch, but that is not essential.
+# The run method will return the `Result` that contains all runs performed.
+
+rs = RandomSearch(configspace=w.get_configspace(),
+                  run_id='example1', nameserver='127.0.0.1',
+                  min_budget=int(args.budget), max_budget=int(args.budget))
+res = rs.run(n_iterations=args.n_iterations)
+
+# Step 4: Shutdown
+# After the optimizer run, we must shutdown the master and the nameserver.
+rs.shutdown(shutdown_workers=True)
+NS.shutdown()
+
+# Step 5: Analysis
+# Each optimizer returns a hpbandster.core.result.Result object.
+# It holds information about the optimization run like the incumbent (=best) configuration.
+# For further details about the Result object, see its documentation.
+# Here we simply print out the best config and some statistics about the performed runs.
+id2config = res.get_id2config_mapping()
+incumbent = res.get_incumbent_id()
+
+print('Best found configuration:', id2config[incumbent]['config'])
+
+
+# Plots the performance of the best found validation error over time
+all_runs = res.get_all_runs()
+# Let's plot the observed losses grouped by budget,
+import hpbandster.visualization as hpvis
+
+hpvis.losses_over_time(all_runs)
+
+import matplotlib.pyplot as plt
+plt.savefig("random_search.png")
+
+# TODO: retrain the best configuration (called incumbent) and compute the test error
