da35d150726d64ebf9a0e666697beffc207819e2
hueglem <hueglem@informatik.uni-freiburg.de>
2018-11-20 10:44:30 +0100

    changed number of test episodes

--- exercise3_R_NR/test_agent.py
+++ exercise3_R_NR/test_agent.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # important: don't set rendering to False for evaluation (you may get corrupted state images from gym)
     rendering = True                      
     
-    n_test_episodes = 10                  # number of episodes to test
+    n_test_episodes = 15                  # number of episodes to test
 
     # TODO: load agent
     # agent = Model(...)
